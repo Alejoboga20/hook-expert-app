0859903bd7d75a0b2522ffa1240adcb6fb8b92a4
alejoboga <alejoboga20@gmail.com>
2021-04-10 20:11:35 -0300

    Creating useFetch hook

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 //import HookApp from './HookApp';
 //import CounterApp from './components/01-useState/CounterApp';
 //import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'));
