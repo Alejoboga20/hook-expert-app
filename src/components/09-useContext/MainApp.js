26b3ed076bb11f94801db665049cab947d83f402
alejoboga <alejoboga20@gmail.com>
2021-04-14 15:00:02 -0300

    Creating context

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import { UserContext } from './UserContext';
 
 const MainApp = () => {
+  const user = {
+    id: 1234,
+    name: 'Alejo',
+    email: 'test@gmail.com'
+  };
+
   return (
     <div>
-      <AppRouter />
+      <UserContext.Provider value={user}>
+        <AppRouter />
+      </UserContext.Provider>
     </div>
   );
 };
