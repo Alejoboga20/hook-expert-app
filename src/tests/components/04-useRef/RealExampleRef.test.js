aa0e8d4d0967c965cea8b3a71feceb80569f3fae
alejoboga <alejoboga20@gmail.com>
2021-04-17 17:59:21 -0300

    Tests for useRef

---
+++ src/tests/components/04-useRef/RealExampleRef.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import RealExampleRef from '../../../components/04-useRef/RealExampleRef';
+
+describe('useRef tests', () => {
+  const wrapper = shallow(<RealExampleRef />);
+
+  test('should display properly', () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('MultipleCustomHooks').exists()).toBe(false);
+  });
+
+  test('should show the component MultipleCustomHooks', () => {
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('MultipleCustomHooks').exists()).toBe(true);
+  });
+});
