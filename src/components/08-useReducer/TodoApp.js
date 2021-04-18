768585162a398509d33788e432000ce7cdf38c5a
alejoboga <alejoboga20@gmail.com>
2021-04-18 18:57:18 -0300

    Refactoring todoApp

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,29 +1,28 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { todoReducer } from './todoReducer';
+
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 import './styles.css';
-import useForm from '../../hooks/useForm';
-import TodoList from './TodoList';
 
 const init = () => {
   return JSON.parse(localStorage.getItem('todos')) || [];
 };
 
-const TodoApp = () => {
+export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
-
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: ''
-  });
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
   const handleDelete = (todoId) => {
-    dispatch({
+    const action = {
       type: 'delete',
       payload: todoId
-    });
+    };
+
+    dispatch(action);
   };
 
   const handleToggle = (todoId) => {
@@ -33,65 +32,31 @@ const TodoApp = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (description.trim().length <= 1) {
-      return;
-    }
-
-    const newTodo = {
-      id: new Date().getTime(),
-      description: description,
-      done: false
-    };
-
-    const action = {
+  const handleAddTodo = (newTodo) => {
+    dispatch({
       type: 'add',
       payload: newTodo
-    };
-
-    dispatch(action);
-    reset();
+    });
   };
 
   return (
     <div>
-      <h1>Todo App {todos.length}</h1>
+      <h1>TodoApp ( {todos.length} ) </h1>
       <hr />
 
       <div className='row'>
         <div className='col-7'>
           <TodoList
             todos={todos}
-            handleToggle={handleToggle}
             handleDelete={handleDelete}
+            handleToggle={handleToggle}
           />
         </div>
 
         <div className='col-5'>
-          <h4>Add Task</h4>
-          <hr />
-
-          <form className='form-control' onSubmit={handleSubmit}>
-            <div>
-              <input
-                onChange={handleInputChange}
-                type='text'
-                name='description'
-                placeholder='Task'
-                autoComplete='off'
-                value={description}
-              />
-            </div>
-            <button className='btn btn-outline-primary mt-1' type='submit'>
-              Add
-            </button>
-          </form>
+          <TodoAdd handleAddTodo={handleAddTodo} />
         </div>
       </div>
     </div>
   );
 };
-
-export default TodoApp;
