fc8dde1b0a92881be7cd313894256b8b547bde72
alejoboga <alejoboga20@gmail.com>
2021-04-19 09:11:23 -0300

    Tests for TodoApp

---
+++ src/tests/components/08-useReducer/TodoApp.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { TodoApp } from '../../../components/08-useReducer/TodoApp';
+import { demoTodos } from '../../fixtures/demoTodos';
+import { act } from '@testing-library/react';
+
+describe('TodoApp Tests', () => {
+  const wrapper = shallow(<TodoApp />);
+  Storage.prototype.setItem = jest.fn(() => {});
+
+  test('should display properly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should add a Todo', () => {
+    const wrapper = mount(<TodoApp />);
+    act(() => {
+      wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodos[0]);
+      wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodos[1]);
+    });
+
+    expect(wrapper.find('h1').text().trim()).toBe(2);
+    expect(localStorage.setItem).toBeCalledTimes(2);
+  });
+
+  test('should delete a Todo', () => {
+    wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodos[0]);
+    expect(wrapper.find('h1').text().trim()).toBe(1);
+    wrapper.find('TodoList').prop('handleDelete')(demoTodos[0].id);
+    expect(wrapper.find('h1').text().trim()).toBe(0);
+  });
+});
